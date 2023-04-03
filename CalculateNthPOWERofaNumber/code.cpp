eda8edf2dd1d6c6e327459b9a4617031b3e3de68
Gabriel Acassio <100379848+GacAcassio@users.noreply.github.com>
2023-04-02 23:58:06 -0400

    Create code.cpp

---
+++ CalculateNthPOWERofaNumber/code.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+
+int main()   //DO NOT change the 'main' signature
+{
+    //Fill code here
+    int input, n;
+    cout << "Enter the number" << endl;
+    cin >> input;
+    cout << "Enter power value" << endl;
+    cin >> n;
+    cout << pow(input, n) << endl;
+    return 0;
+}
